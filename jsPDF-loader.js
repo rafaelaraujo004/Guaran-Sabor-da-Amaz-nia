07256ac771a80b3588a268e2918921fc9eac93e2
Rafael Araújo <122177980+rafaelaraujo004@users.noreply.github.com>
2025-08-29 09:13:56 -0300

    Add files via upload

--- jsPDF-loader.js
+++ jsPDF-loader.js
@@ -1,9 +1,9 @@
 // Adiciona jsPDF via CDN para geração de PDF
 (function(){
-  if (!window.jspdfLoaded) {
-    var script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js';
-    script.onload = function() { window.jspdfLoaded = true; };
-    document.head.appendChild(script);
-  }
+	if (!window.jspdfLoaded) {
+		var script = document.createElement('script');
+		script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js';
+		script.onload = function() { window.jspdfLoaded = true; };
+		document.head.appendChild(script);
+	}
 })();
